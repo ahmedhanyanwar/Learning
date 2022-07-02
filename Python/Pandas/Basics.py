85884cc258a31f6d322d4c3d268c0392eaae8f43
Ahmed Hany <ahmedhanyanwar12@gmail.com>
2022-07-03 00:16:29 +0200

    Pandas

---
+++ Python/Pandas/Basics.py
@@ -0,0 +1,86 @@
+import pandas as pd
+
+###   Reading file  & Sorting/Describing Data  & Making changes to the data & Saving our Data 
+#### Video link : https://www.youtube.com/watch?v=vmEHCJofslg
+
+#####################################   Reading file
+df = pd.read_csv('pokemon_data.csv')
+df_txt = pd.read_csv('pokemon_data.txt',delimiter='\t') ## to separate data
+df_xlsx = pd.read_excel('pokemon_data.xlsx')
+
+firstPart =  df.head(3) ## subset of df 
+lastPart =  df.tail(3) ## subset of df 
+# print(df_txt.head(2))
+
+### Read headers
+col = df.columns  ## like  name type ... etc
+
+### Read each columns
+col = df['Name'][0:5] ## [0:5] read part of them
+col = df.Name[0:5]    ## [0:5] read part of them
+cols = df[['Name','Speed']][0:5]  ## read multiple column
+# print(cols)
+
+### Read each Row
+toalRow = df.iloc   # iloc == Index LOCation
+row = df.iloc[1]    # second Row
+# print(toalRow[1:4])
+
+### Read a specific position (Row,col)
+item =df.iloc[2,1]
+# print(item)
+
+###  To find specific row depends on multiple condition
+###  we can say we search about something 
+fireRow = df.loc[df['Type 1'] == 'Fire']
+# print(fireRow)
+
+### acess data row by row
+# for index ,row in df.iterrows():
+    # print(index,row['Name'])   ## Print name in each row
+
+
+
+##########      Sorting/Describing Data    ########
+###################################################
+##    df.describe()   Gives us higher calcuation like mean & std & count & min & max
+des = df.describe()
+
+##Sort
+sort_aToz = df.sort_values('Name') ##  Sort it alphabetically from A to Z
+sort_zToa = df.sort_values('Name',ascending=False) ##  Sort it alphabetically from Z to A
+
+## Sort by multiple things == sort by first then second
+sort_mult = df.sort_values(['Type 1','HP'])
+sort_multde = df.sort_values(['Type 1','HP'],ascending=[True,False]) ##  Sort Type1 alphabetically from A to Z and HP from High to Low
+
+
+###################################################
+
+
+##########      Making changes to the data    ########
+######################################################
+## add another column called tatal and assign it's valuse
+df['Total'] = df['HP'] + df['Attack'] + df['Defense'] + df['Sp. Atk'] + df['Sp. Def'] + df['Speed']
+df = df.drop(columns=['Total'])  ## To delete columns 
+
+## Another way to add colums with easiest way
+HPIndex  =  df.columns.get_loc('HP')
+speedIndex = df.columns.get_loc('Speed')
+df['Total'] = df.iloc[:,HPIndex:speedIndex+1].sum(axis=1)  #axis = 1 because we want sum on X axis
+
+### Reorder colums total   before  HP
+cols = list(df.columns)
+df = df[cols[0:HPIndex] + [cols[-1]]+cols[HPIndex:12]]
+
+print(df.head(5))
+######################################################
+
+###############      Saving our Data    ##############
+######################################################
+# df.to_csv('Modified.csv',index=False) ## index = false to remove index from new file
+# df.to_excel('Modified.xlsx',index=False)
+# df.to_csv('Modified.txt',index=False,sep='\t')  ## sep ='\t' to seprate output
+
+######################################################
+
---
+++ Python/Pandas/Basics2.py
@@ -0,0 +1,87 @@
+from typing import Type
+import pandas as pd
+# from pandas import read_csv as readpd
+
+#############################################
+#'^Pi[a-z]*' contain pi followed by letter from a to z and {*} mean zero or more elements and {^} to be in first not a middle 
+#############################################
+
+
+################   Filtering data (LOC) #################
+#########################################################
+##### in filter pandas we use    & | ~ instead of and or not
+#####    and or not will give error
+df = pd.read_csv('Modified.csv') #data frame ###pokemon_data.csv
+
+filter = df.loc[(df['Type 1']=='Grass') & (df['Type 2'] =='Poison')&(df['HP']>70)]
+
+###  the new filter data save old index in it to reset index we will use
+
+# filter = filter.reset_index()   ### this will make new index and save old index
+# filter = filter.drop(columns='index')
+filter = filter.reset_index(drop=True,inplace=True)  ##  ==  filter = filter.reset_index() + filter.drop(colums='index')
+
+### If I want name which cotain Mega -- turn it to string then use contain
+conMega = df.loc[df['Name'].str.contains('Mega')]
+
+### If I want check if Type1 == Grass or Fire   -- I will use re library
+## Reglar Expression  it is super powerful in filtering data based on certain textual patterns
+import re   
+fireGrass = df.loc[df['Type 1'].str.contains('Fire|Grass',regex=True)]
+#### It is fire and grass with small letter but it works because of re.I
+fireGrass = df.loc[df['Type 1'].str.contains('fire|grass',flags=re.I,regex=True)]  
+
+####  contain pi followed by letter from a to z and {*} mean zero or more elements and {^} to be in first not a middle 
+nameWithPi = df.loc[df['Name'].str.contains('^Pi[a-z]*',flags=re.I,regex=True)]
+#########################################################
+
+
+
+################   Conditional changes  #################
+#########################################################
+### df.loc[row==fire,colum of Type 1] = 'Flamer'
+df.loc[df['Type 1']=='Fire','Type 1'] = 'Flamer'
+df.loc[df['Type 1']=='Flamer','Type 1'] = 'Fire'
+
+###   convert legendary of rows thats Type 1 == Fire to True
+df.loc[df['Type 1']=='Fire','Legendary'] = True
+
+###  Multiple changes
+df.loc[df['Total'] > 500,['Generation','Legendary']] = 'Test value'
+df.loc[df['Total'] > 500,['Generation','Legendary']] = ['Test1','Test2']
+
+#########################################################
+
+
+#######    Aggregate Statistics (Groupby)   #############
+#########################################################
+df = pd.read_csv('Modified.csv')
+
+## I want find the type 1 average to see who has high Defense
+## it grouped all different types in type 1
+highDef = df.groupby('Type 1').mean().sort_values('Defense',ascending=False)
+highAtt  = df.groupby('Type 1').mean().sort_values('Attack',ascending=False)
+
+## I want to know count [number of pokemon in type 1]
+num = df.groupby(['Type 1']).count() # but there are som NAN in some colums so I will do:
+
+##  To concern in count rather than blanks
+df['Count'] = 1
+num  = df.groupby(['Type 1']).count()['Count']
+df = df.drop(columns='Count') ## I will remove count colums because I don't need it now
+# print(num)
+#########################################################
+
+
+#######    Working with large amounts of data   #############
+#############################################################
+new_df = pd.DataFrame(columns=df.columns) ### empty data frame has same colums as df
+for df in pd.read_csv('Modified.csv',chunksize=5):
+    results = df.groupby(['Type 1']).count()
+    new_df = pd.concat([new_df,results])
+
+    # print("CHUNKS = ")
+    # print(df)  ### while print 5 by 5 rows     
+print(new_df)  #### now we rdused data by another useful info
+
+#############################################################
